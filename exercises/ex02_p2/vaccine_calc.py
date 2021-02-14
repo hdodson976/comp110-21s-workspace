0955e228abb6f44ffa39a92e577660e26762c097
Hailey Dodson <hdodson@email.unc.edu>
2021-02-14 14:55:52 -0500

    Exercise 2 Part 2 Progress

--- exercises/ex02_p2/vaccine_calc.py
+++ exercises/ex02_p2/vaccine_calc.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime, timedelta
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730142451"
 
 
 def main() -> None:
@@ -12,14 +12,31 @@ def main() -> None:
     doses_per_day: int = int(input("Doses per day: "))
     target: int = int(input("Target percent vaccinated: "))
     # TODO 2: Call days_to_target and store result in a variable.
+    remaining_days: int = days_to_target(population, doses, doses_per_day, target)
     # TODO 4: Call future_date and store result in a variable.
+    final_date: str = future_date(remaining_days)
     # TODO 5: Print the expected output using the variables above.
+    first_half: str = "We will reach " + str(target) + "% vaccination in " + str(remaining_days) + " days,"
+    second_half: str = " which falls on " + final_date.strftime("%B %d, %Y") + "."
+    print(first_half + second_half)
 
 
 # TODO 1: Define days_to_target function
+def days_to_target(population: int, doses: int, doses_per_day: int, target: int) -> int:
+    "Uses input to calculate the days needed to reach the target percentage."
+    percent_of_pop: float = float(population) * (float(target) / 100)
+    numerator: float = (percent_of_pop - (float(doses) / 2)) * 2
+    enitre_calc: float = numerator / float(doses_per_day)
+    rounded: int = round(enitre_calc)
+    return rounded
 
 
 # TODO 3: Define future_date function
+def future_date(remaining_days: int) -> str:
+    today: datetime = datetime.today()
+    remaining: timedelta = timedelta(remaining_days)
+    end_date: datetime = today + remaining
+    return end_date
 
 
 if __name__ == "__main__":
