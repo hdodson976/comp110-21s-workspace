a03419412d35e60b715f5202c5b186e39b33e47d
Hailey Dodson <hdodson@email.unc.edu>
2021-02-28 16:15:26 -0500

    project 1 progress

---
+++ lessons/sum_algo.py
@@ -0,0 +1,20 @@
+"""Examples of list and loop algorithm."""
+
+def sum_algo(xs: ist[int]) -> int:
+    """Summation of input list is returned."""
+    # 1. Initialize total and index i to 0
+    total: int = 0
+    i: int = 0
+    # 2. While index i is valid, meaning i < len(xs)
+    while i < len(xs): 
+        #   2.True) Take xs[i] and add to total
+        total += xs[i]
+        #   2.True) Increase i by 1, moving it to the next index
+        i += 1
+
+    #   2.False) Result in stored total variable
+    return total
+
+# Example usage of the sum_algo function 
+odd: list[int] = [1, 3, 5, 7]
+odds_sum: int = sum_algo(odds)
---
+++ projects/cyoa.py
@@ -0,0 +1,160 @@
+"""Virtual MMA Fight, player fights a virutal opponents and whoever's health reaches 0 first loses."""
+
+from random import randint
+
+__author__ = "730142451"
+
+
+
+# main function, start with greet function.
+def main() -> None: 
+    greet()
+    pathway_selection()
+
+# Global variables
+player: str = input("Welcome to Virtual MMA. What is your name? ")
+# Points tracks the players health throughout the fight, will change depending on
+# choices and the random reponse back. 
+points: int = 100
+opponent_points: int = 100
+kick: int = randint(10, 20)
+punch: int = randint(1, 20)
+sad_face: str = "\U0001F622"
+happy_face: str = "\U0001F642"
+dizzy_face: str = "\U0001F635\U0001F4AB"
+
+
+# greet function, explain how the moves and point system works."
+def greet() -> None: 
+    print(f"{player}, in this game, you will engage in a MMA fight with a virutal opponent.")
+    print(f"You and your opponent will begin the fight with {points} health points each.")
+    print("Each round you will choose between blocking, kicking, and punching.")
+    print("A combination of your selection and you opponent's selection will determine how each players health is impacted.")
+    print("Whoever's health points reaches 0 first loses")
+    
+
+# 3 different pathways for the play to select from
+def pathway_selection () -> None:
+    """Player selects whether to surrender, have a practice round, or begin the fight."""
+    pathway: str = input("Would you like to surrender, practice, or begin? ")
+    if pathway == "surrender":
+        print(f"Goodbye {player}.")
+    else: 
+        if pathway == "practice":
+            practice()
+        else: 
+            fight()
+
+
+def practice() -> None:
+    practice_move: str = input("Would you like to punch or kick?")
+    # The virutal opponents move will be randomly selected. 
+    opponent_move: int = randint(1, 2)
+    if opponent_move == 1: 
+        global points
+        points = points - punch
+        print(f"{player}'s Health: {points}")
+    else: 
+        if opponent_move == 2: 
+            points = points - kick
+            print(f"{player}'s Health: {points}")
+    if practice_move == "punch":
+        global opponent_points
+        opponent_points = opponent_points - punch
+        print(f"Opponent's Health: {opponent_points}")
+    else: 
+        if practice_move == "kick":
+            opponent_points = opponent_points - kick
+            print(f"Opponent's Health: {opponent_points}")
+        else: 
+            print(f"Opponent's Health: {opponent_points}")
+    pathway_selection()
+
+
+# First round, then offer the player the option to surrender.
+def fight() -> None: 
+    global points
+    points = 100
+    global opponent_points
+    opponent_points = 100
+    while points > 50 and opponent_points > 1: 
+        opponent_move: int = randint(1, 2)
+        move: str = input("Would you like to punch or kick? ")
+        if opponent_move == 1: 
+            points = points - punch
+            print(f"{player}'s Health: {points}")
+            if move == "punch":
+                opponent_points = opponent_points - punch
+                print(f"Opponent's Health: {opponent_points}")
+            else: 
+                if move == "kick":
+                    opponent_points = opponent_points - kick
+                    print(f"Opponent's Health: {opponent_points}")
+                else: 
+                    print(f"Opponent's Health: {opponent_points}")
+        else: 
+            points = points - kick
+            print(f"{player}'s Health: {points}")
+            if move == "punch":
+                opponent_points = opponent_points - punch
+                print(f"Opponent's Health: {opponent_points}")
+            else: 
+                if move == "kick":
+                    opponent_points = opponent_points - kick
+                    print(f"Opponent's Health: {opponent_points}")
+                else: 
+                    print(f"Opponent's Health: {opponent_points}")
+        if opponent_points < 0:
+            print(f"You win {happy_face}!")
+            main()
+    check_in(points)
+    
+        
+
+def check_in(x = int) -> None: 
+    answer: str = input(f"Your health is getting low {dizzy_face}, do you want to surrender or continue? ")
+    if answer == "surrender":
+        print(f"Goodbye {player}")
+    else: 
+        if answer == "continue":
+            global opponent_points
+            while x > 1 and opponent_points > 1: 
+                opponent_move: int = randint(1, 2)
+                move: str = input("Would you like to punch or kick? ")
+                if opponent_move == 1: 
+                    x = x - punch
+                    print(f"{player}'s Health: {x}.")
+                    if move == "punch":
+                        opponent_points = opponent_points - punch
+                        print(f"Opponent's Health: {opponent_points}")
+                    else: 
+                        if move == "kick":
+                            opponent_points = opponent_points - kick
+                            print(f"Opponent's Health: {opponent_points}")
+                        else: 
+                            print(f"Opponent's Health: {opponent_points}")
+                else: 
+                    x = x - kick
+                    print(f"{player}'s Health: {x}")
+                    if move == "punch":
+                        opponent_points = opponent_points - punch
+                        print(f"Opponent's Health: {opponent_points}")
+                    else: 
+                        if move == "kick":
+                            opponent_points = opponent_points - kick
+                            print(f"Opponent's Health: {opponent_points}")
+                        else: 
+                            print(f"Opponent's Health: {opponent_points}")
+        else:
+            print(f"Goodbye {player}")
+    if x < 1:
+        print(f"You lose {sad_face}")
+    else: 
+        if opponent_points < 1:
+            print(f"You win {happy_face}!")
+
+
+#Call main function
+if __name__ == "__main__":
+    main()
+
